572e3f7a431e65ac1042fb1ad04bdf95a1173982
affu3579 <64513295+affu3579@users.noreply.github.com>
2020-07-06 10:57:48 +0530

    Update Hai.java

--- Hai.java
+++ Hai.java
@@ -1,8 +1,12 @@
 Class Hai
 {
+void For(){
+System.out.println("for done");
+}
 public static void main(String[] args)
 {
 System.out.println("hai...viwers);
 System.out.println("hello guys");
+
 }
 }
